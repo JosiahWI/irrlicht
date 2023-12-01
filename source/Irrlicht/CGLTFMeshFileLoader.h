5248deda0159db3faa321b1199c037f7b5fafedc
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-11-30 22:05:43 -0500

    Remove redundant function

--- source/Irrlicht/CGLTFMeshFileLoader.h
+++ source/Irrlicht/CGLTFMeshFileLoader.h
@@ -102,8 +102,6 @@ private:
 		 */
 		float getScale() const;
 
-    int getComponentType(const std::size_t accessorIdx) const;
-
 		std::size_t getElemCount(const std::size_t accessorIdx) const;
 
 		std::size_t getByteStride(const std::size_t accessorIdx) const;
